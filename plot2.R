0ee7b58fc994bae85f9ef8dfa19e017908651c0d
eyalalmog <eyalalmog@yahoo.com>
2015-04-12 13:47:33 -0400

    added

---
+++ plot2.R
@@ -0,0 +1,12 @@
+
+setwd("C:\\R\\exploratory\\Data")
+
+dataFile <- "household_power_consumption.txt" 
+FullData <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+subSetData <- FullData[FullData$Date %in% c("1/2/2007","2/2/2007") ,] 
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
+globalActivePower <- as.numeric(subSetData$Global_active_power) 
+png("plot2.png", width=480, height=480) 
+plot(datetime, globalActivePower, type="l", xlab="Day Of Week", ylab="Global Active Power (KW)") 
+dev.off() 
